8401c46b763be2d5089a720427c9a32504767c5a
Osama Basheer <838@holbertonschool.com>
2019-09-25 17:49:44 +0000

    [FEATURE] Add Pall function

---
+++ pall.c
@@ -0,0 +1,14 @@
+#include "pancakes.h"
+/**
+ * pall - print all of the stack
+ * @stack: pointer to stack
+ * @line_number: line number
+ * Return: void
+ */
+void pall(stack_t **stack, unsigned int line_number)
+{
+	stack_t *traverse = *stack;
+	(void)line_number;
+	for (; traverse; traverse = traverse->next)
+		printf("%d\n", traverse->n);
+}
