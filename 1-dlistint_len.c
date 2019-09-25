acd4cc5ecacddad2e4ce748485019f309f54e194
Osama Basheer <838@holbertonschool.com>
2019-09-25 19:21:50 +0000

    Add 1-dlistint_len.c

---
+++ 1-dlistint_len.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * dlistint_len - returns the number of elements in dlistint_t list
+ * @h: doubly linked list
+ * Return: number of elements in list
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	int i;
+
+	for (i = 0; h != NULL; i++)
+	{
+		h = h->next;
+	}
+	return (i);
+}
