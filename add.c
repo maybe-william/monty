f4c961d15596971366cae18060a300d2b6483abf
Osama Basheer <838@holbertonschool.com>
2019-09-26 20:43:54 +0000

    [TASK] Add add.c

--- add.c
+++ add.c
@@ -7,6 +7,21 @@
  */
 void add(stack_t **head, unsigned int linum)
 {
-	(void)head;
-	(void)linum;
+	int x = 0;
+	int y = 0;
+	int z = 0;
+
+	if (!head || !(dlistint_len(*head) > 2))
+	{
+		add_error(linum);
+		last_status(-1);
+		return;
+	}
+
+	x = get_dnodeint_at_index(*head, 0)->n;
+	y = get_dnodeint_at_index(*head, 1)->n;
+	z = x + y;
+	get_dnodeint_at_index(*head, 1)->n = z;
+
+	delete_dnodeint_at_index(head, 0);
 }
