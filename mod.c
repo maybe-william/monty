92a5c7d826b815d88babeeba68125f047ffbcd1d
william <818@holbertonschool.com>
2019-09-27 01:44:34 +0000

    [FEATURE] Implement mod

--- mod.c
+++ mod.c
@@ -7,6 +7,27 @@
  */
 void mod(stack_t **head, unsigned int linum)
 {
-	(void)head;
-	(void)linum;
+	int x = 0;
+	int y = 0;
+	int z = 0;
+
+	if (!head || !(dlistint_len(*head) >= 2))
+	{
+		mod_error(linum);
+		last_status(-1);
+		return;
+	}
+
+	x = get_dnodeint_at_index(*head, 0)->n;
+	y = get_dnodeint_at_index(*head, 1)->n;
+	if (y == 0)
+	{
+		div_zero_error(linum);
+		last_status(-1);
+		return;
+	}
+	z = x % y;
+	get_dnodeint_at_index(*head, 1)->n = z;
+
+	delete_dnodeint_at_index(head, 0);
 }
